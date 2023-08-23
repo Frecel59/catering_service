7ec8fb698e4f717cf7f1597ac9e275fc93d78f96
cedric lecerf <lecerf.c83@gmail.com>
2023-08-23 14:46:00 +0200

    ajout Clean_data.py

---
+++ Data_cleaning/Clean_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def concatenate_data():
+    return
+
+def clean_data():
+    # Appeler la fonction pour concaténer la data
+    df = concatenate_data()
+    return
+
+if __name__ == '__main__':
+    # Appeler la fonction pour le dataframe final
+    print(clean_data().info())
