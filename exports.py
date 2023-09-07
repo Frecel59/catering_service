096a693edc885d60deb1a54a1e1bb9823dd92ebc
cedric lecerf <lecerf.c83@gmail.com>
2023-09-07 15:31:19 +0200

    MAJ css

--- exports.py
+++ exports.py
@@ -1,8 +1,27 @@
 import streamlit as st
 
+
 def main():
+    # Charger le contenu du fichier CSS
+    with open('style.css', 'r') as css_file:
+        css = css_file.read()
+
+    # Afficher le contenu CSS dans la page Streamlit
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+    #########################################################################
+    #########################################################################
+
     st.title("Exportations des Fichiers")
-    st.write("Contenu de la page d'export des fichiers.")
+
+    # Utiliser le séparateur horizontal avec la classe CSS personnalisée
+    st.markdown('<hr class="custom-separator">', unsafe_allow_html=True)
+
+    #########################################################################
+    ############################## EN COURS #################################
+    #########################################################################
+
+
 
 if __name__ == "__main__":
     main()
--- predictions.py
+++ predictions.py
@@ -1,8 +1,25 @@
 import streamlit as st
 
+
 def main():
+    # Charger le contenu du fichier CSS
+    with open('style.css', 'r') as css_file:
+        css = css_file.read()
+
+    # Afficher le contenu CSS dans la page Streamlit
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+    #########################################################################
+    #########################################################################
+
     st.title("Prédictions")
-    st.write("Contenu de la page de prédictions.")
+
+    # Utiliser le séparateur horizontal avec la classe CSS personnalisée
+    st.markdown('<hr class="custom-separator">', unsafe_allow_html=True)
+
+    #########################################################################
+    ############################## EN COURS #################################
+    #########################################################################
 
 if __name__ == "__main__":
     main()
