95eb94018c54c3e1c4d3f644fd36ae1db84e7a77
cedric lecerf <lecerf.c83@gmail.com>
2023-09-08 14:04:17 +0200

    externalisation du rapport excel

---
+++ Analyses/excel_generation.py
@@ -0,0 +1,86 @@
+import io
+import pandas as pd
+import xlsxwriter
+
+
+
+def generate_excel_report(result_df, start_date, end_date):
+    # Créer un objet BytesIO pour stocker les données Excel
+    output = io.BytesIO()
+
+    # Multipliez la colonne % par 100 pour obtenir la représentation en pourcentage correcte
+    result_df['%'] /= 100
+
+    # Utiliser Pandas pour sauvegarder le DataFrame au format Excel dans l'objet BytesIO
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        result_df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+        # Accédez à la feuille Excel générée pour formater les colonnes
+        worksheet = writer.sheets['Sheet1']
+
+        # Créer un format personnalisé pour les en-têtes de colonnes
+        header_format = writer.book.add_format({
+            'bold': True,         # En gras
+            'text_wrap': True,    # Saut de ligne automatique pour les en-têtes longs
+            'align': 'center',    # Alignement au centre
+            'valign': 'vcenter',  # Alignement vertical au centre
+            'fg_color': '#D7E4BC',  # Couleur de fond
+            'border': 1,          # Bordures
+            'font_size': 11       # Taille de police
+        })
+
+        # Appliquer le format aux en-têtes de colonnes
+        for col_num, value in enumerate(result_df.columns.values):
+            worksheet.write(0, col_num, value, header_format)
+
+        # Créez un format personnalisé pour la colonne A (Type)
+        custom_number_format_A = writer.book.add_format({
+            'font_size': 11,  # Taille de police
+            'bold': True  # Police en gras
+        })
+
+        # Appliquer le format correspondant
+        worksheet.set_column('A:A', len('Type') + 2, custom_number_format_A)
+
+
+        # Créez un format personnalisé pour la colonne B (Nbr Couverts)
+        custom_number_format_B = writer.book.add_format({
+            'num_format': '#,##0',  # Format nombre avec séparateur de milliers
+            'font_size': 11  # Taille de police
+        })
+
+        # Appliquer le format correspondant
+        worksheet.set_column('B:B', len('Nbr Couverts') + 2, custom_number_format_B)
+
+        # Créez un format personnalisé pour la colonne C (%)
+        custom_percent_format_C = writer.book.add_format({
+            'num_format': '0.00%',  # Format pourcentage avec 2 décimales
+            'font_size': 11  # Taille de police
+        })
+
+        # Appliquer le format correspondant
+        worksheet.set_column('C:C', len('%') + 2, custom_percent_format_C)
+
+        # Créez un format personnalisé pour la colonne D (Total Additions €)
+        custom_compte_format_D = writer.book.add_format({
+            'num_format': '#,##0.00 €',  # Format comptabilité avec 2 décimales et le symbole €
+            'font_size': 11  # Taille de police
+        })
+
+        # Appliquer le format correspondant
+        worksheet.set_column('D:D', len('Total Additions €') + 2, custom_compte_format_D)
+
+        # Créez un format personnalisé pour la colonne E (Panier moyen €)
+        custom_compte_format_E = writer.book.add_format({
+            'num_format': '#,##0.00 €',  # Format comptabilité avec 2 décimales et le symbole €
+            'font_size': 11  # Taille de police
+        })
+
+        # Appliquer le format correspondant
+        worksheet.set_column('E:E', len('Panier moyen €') + 2, custom_compte_format_E)
+
+
+    # Définir le point de départ pour la lecture
+    output.seek(0)
+
+    return output
