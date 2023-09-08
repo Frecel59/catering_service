83ab06fbbc878cef74578ab2732297f15c940a8e
cedric lecerf <lecerf.c83@gmail.com>
2023-09-08 16:00:52 +0200

    optimisation du code

--- Analyses/graph.py
+++ Analyses/graph.py
@@ -1,20 +1,31 @@
+# Importation des bibliothèques nécessaires
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Créez une fonction pour afficher un graphique avec filtre
-def show_grouped_data(group_by, data_type, group_by_option, data_type_option, month, filtered_df):
-        days_order = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+def show_grouped_data(group_by, data_type, group_by_option, data_type_option, \
+    month, filtered_df):
+        days_order = ['Lundi',
+                      'Mardi',
+                      'Mercredi',
+                      'Jeudi',
+                      'Vendredi',
+                      'Samedi',
+                      'Dimanche']
 
         df_grouped = filtered_df.copy()
         if group_by == 'Jour':
-            df_grouped['Jour'] = pd.Categorical(df_grouped['Jour'], categories=days_order, ordered=True)
+            df_grouped['Jour'] = pd.Categorical(df_grouped['Jour'], \
+                categories=days_order, ordered=True)
             grouped = df_grouped.groupby('Jour')[data_type].sum().reset_index()
 
         elif group_by == 'Mois et Jour' and month:
             df_grouped['mois'] = df_grouped['Date'].dt.to_period("M")
-            df_grouped['Jour'] = pd.Categorical(df_grouped['Jour'], categories=days_order, ordered=True)
-            grouped = df_grouped[df_grouped['mois'].dt.strftime('%m/%Y') == month].groupby(['mois', 'Jour'])[data_type].sum().reset_index()
+            df_grouped['Jour'] = pd.Categorical(df_grouped['Jour'], \
+                categories=days_order, ordered=True)
+            grouped = df_grouped[df_grouped['mois'].dt.strftime('%m/%Y') \
+                == month].groupby(['mois', 'Jour'])[data_type].sum().reset_index()
 
         fig, ax = plt.subplots()
         ax.bar(grouped['Jour'], grouped[data_type])
