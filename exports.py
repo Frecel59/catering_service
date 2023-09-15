9e20f3856c2e7d532e59c21a01344b33915f5fa4
cedric lecerf <lecerf.c83@gmail.com>
2023-09-15 16:45:51 +0200

    mise en fonction de l'envoie de fichiers vers GCP

--- exports.py
+++ exports.py
@@ -1,27 +1,64 @@
 import streamlit as st
+import pandas as pd
+from dotenv import load_dotenv
+from config import Config
+from gcp import get_storage_client
 
+load_dotenv()
+
+# Utilisez la fonction get_storage_client pour obtenir le client et le bucket
+client, bucket = get_storage_client()
 
 def main():
-    # Charger le contenu du fichier CSS
+    # Charger le contenu du fichier CSS personnalisé
     with open('style.css', 'r') as css_file:
         css = css_file.read()
 
-    # Afficher le contenu CSS dans la page Streamlit
+    # Incorporer le CSS personnalisé dans l'application
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-    #########################################################################
-    #########################################################################
-
     st.title("Exportations des Fichiers")
-
-    # Utiliser le séparateur horizontal avec la classe CSS personnalisée
     st.markdown('<hr class="custom-separator">', unsafe_allow_html=True)
 
-    #########################################################################
-    ############################## EN COURS #################################
-    #########################################################################
+    # Ajoutez un champ d'envoi de fichiers pour le fichier Brasserie
+    st.header("Fichier Brasserie")
+    uploaded_file_brasserie = st.file_uploader("Sélectionnez un fichier Excel (xlsx) à envoyer (Brasserie)", type=["xlsx"])
 
+    if uploaded_file_brasserie:
+        file_name = uploaded_file_brasserie.name
+        target_folder = "COVERS_BRASSERIE"  # Vous pouvez déterminer le dossier cible en fonction des préférences de l'utilisateur
 
+        # Lire le fichier Excel en tant que DataFrame
+        df = pd.read_excel(uploaded_file_brasserie)
+
+        # Créez un objet blob pour le fichier dans le bucket
+        blob = bucket.blob(f"{target_folder}/{file_name}")
+
+        # Envoyez le DataFrame directement au blob au format CSV
+        csv_data = df.to_csv(index=False)
+        blob.upload_from_string(csv_data, content_type="application/vnd.ms-excel")  # Spécifiez le type MIME pour les fichiers Excel
+
+        st.success(f"Le fichier {file_name} (Brasserie) a été envoyé avec succès dans le dossier {target_folder} du bucket GCP.")
+
+    # Ajoutez un champ d'envoi de fichiers pour le fichier Snack
+    st.header("Fichier Snack")
+    uploaded_file_snack = st.file_uploader("Sélectionnez un fichier Excel (xlsx) à envoyer (Snack)", type=["xlsx"])
+
+    if uploaded_file_snack:
+        file_name = uploaded_file_snack.name
+        target_folder = "COVERS_SNACK"  # Vous pouvez déterminer le dossier cible en fonction des préférences de l'utilisateur
+
+        # Lire le fichier Excel en tant que DataFrame
+        df = pd.read_excel(uploaded_file_snack)
+
+        # Créez un objet blob pour le fichier dans le bucket
+        blob = bucket.blob(f"{target_folder}/{file_name}")
+
+        # Envoyez le DataFrame directement au blob au format CSV
+        csv_data = df.to_csv(index=False)
+        blob.upload_from_string(csv_data, content_type="application/vnd.ms-excel")  # Spécifiez le type MIME pour les fichiers Excel
+
+        st.success(f"Le fichier {file_name} (Snack) a été envoyé avec succès dans le dossier {target_folder} du bucket GCP.")
 
 if __name__ == "__main__":
     main()
--- gcp.py
+++ gcp.py
@@ -5,29 +5,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-################# Connexion avec GCP ###########################################
-# Récupérer le nom du bucket à partir de la variable d'environnement
-bucket_name = os.getenv('BUCKET_NAME')
+def get_storage_client():
+    # Connexion avec GCP
+    bucket_name = os.getenv('BUCKET_NAME')
+    credentials_dict = {
+        "type": os.getenv('TYPE'),
+        "project_id": os.getenv('PROJECT_ID'),
+        "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+        "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),
+        "client_email": os.getenv('CLIENT_EMAIL'),
+        "client_id": os.getenv('CLIENT_ID'),
+        "auth_uri": os.getenv('AUTH_URI'),
+        "token_uri": os.getenv('TOKEN_URI'),
+        "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
+        "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL')
+    }
 
-# Récupérer les informations d'identification du compte de service à partir du fichier JSON
-credentials_dict = {
-  "type": os.getenv('TYPE'),
-  "project_id": os.getenv('PROJECT_ID'),
-  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
-  "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),
-  "client_email": os.getenv('CLIENT_EMAIL'),
-  "client_id": os.getenv('CLIENT_ID'),
-  "auth_uri": os.getenv('AUTH_URI'),
-  "token_uri": os.getenv('TOKEN_URI'),
-  "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
-  "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL')
-}
+    # Créez des informations d'identification d'objet de compte de service à partir du dictionnaire
+    credentials = service_account.Credentials.from_service_account_info(info=credentials_dict)
 
-# Créer des informations d'identification d'objet de compte de service à partir du dictionnaire
-credentials = service_account.Credentials.from_service_account_info(info=credentials_dict)
+    # Créez un client Storage et retournez-le
+    client = storage.Client(credentials=credentials)
+    bucket = client.get_bucket(bucket_name)
 
-# Créer un client Storage
-client = storage.Client(credentials=credentials)
-
-# Récupérer une référence à votre bucket GCP
-bucket = client.get_bucket(bucket_name)
+    return client, bucket
