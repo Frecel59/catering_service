0e05d785353249c7757dbe7982fddc3cb8b0ffe9
cedric lecerf <lecerf.c83@gmail.com>
2023-09-19 16:04:42 +0200

    MAJ

--- dashboard.py
+++ dashboard.py
@@ -8,21 +8,14 @@ import io
 from gcp import get_storage_client
 
 def get_df_from_gcp():
-        client, bucket = get_storage_client()
-
-        # Chemin vers votre fichier dans le bucket
-        blob_path = "COVERS_BRASSERIE_DF_FINALE/df_finale.xlsx"
-        blob = bucket.blob(blob_path)
-
-        # Téléchargez le fichier dans un objet en mémoire
-        in_memory_file = io.BytesIO()
-        blob.download_to_file(in_memory_file)
-        in_memory_file.seek(0)
-
-        # Lisez le fichier Excel dans un DataFrame
-        df = pd.read_excel(in_memory_file)
-
-        return df
+    client, bucket = get_storage_client()
+    blob_path = "COVERS_BRASSERIE_DF_FINALE/df_finale.xlsx"
+    blob = bucket.blob(blob_path)
+    in_memory_file = io.BytesIO()
+    blob.download_to_file(in_memory_file)
+    in_memory_file.seek(0)
+    df = pd.read_excel(in_memory_file)
+    return df
 
 def main():
     st.title("Tableau de bord - Restauration")
@@ -30,35 +23,46 @@ def main():
     # Récupérer les données
     df = get_df_from_gcp()
 
-    # Sidebar
-    date = st.sidebar.multiselect(
-        "Sélectionnez une Date",
-        options=df["Date"].unique(),
-        default=df["Date"].unique(),
-    )
-    jour = st.sidebar.multiselect(
-        "Sélectionnez un Jour",
-        options=df["Jour"].unique(),
-        default=df["Jour"].unique(),
-    )
-    meteo_12h = st.sidebar.multiselect(
-        "Sélectionnez la Météo à 12h",
-        options=df["Météo 12h"].unique(),
-        default=df["Météo 12h"].unique(),
-    )
+    # Analyse Temporelle
+    st.subheader("Analyse Temporelle")
+    fig1 = px.line(df, x="Date", y=["Nbr total couv. 19h", "Nbr total couv. 12h"], title='Évolution des couverts au fil du temps')
+    st.plotly_chart(fig1)
 
-    df_selection = df[df['Date'].isin(date) & df['Jour'].isin(jour) & df['Météo 12h'].isin(meteo_12h)]
+    # Répartition Jours/Fériés
+    st.subheader("Répartition Jours/Fériés")
+    fig2 = px.histogram(df, x="Jour", y="Nbr total couv.", color="Féries", title='Distribution du nombre de couverts par jour de la semaine')
+    st.plotly_chart(fig2)
 
-    # Afficher le DataFrame filtré
-    with st.expander("Visualiser les Données"):
-        st.dataframe(df_selection)
+    # Analyse Météo
+    st.subheader("Analyse Météo")
+    fig3 = px.box(df, x="Météo 12h", y="Nbr total couv. 12h", title='Distribution des couverts de 12h selon la météo')
+    st.plotly_chart(fig3)
 
-    # Créer un graphique simple (ajustez en fonction de vos besoins)
-    fig = px.bar(df_selection, x="Jour", y="Nombre de clients", title="Nombre de clients par jour")
-    st.plotly_chart(fig)
+    fig4 = px.box(df, x="Méteo 19h", y="Nbr total couv. 19h", title='Distribution des couverts de 19h selon la météo')
+    st.plotly_chart(fig4)
 
+    # Analyse des Serveurs
+    st.subheader("Analyse des Serveurs")
+    fig5 = px.scatter(df, x="Nbr serveurs 12h", y="Nbr total couv. 12h", title='Nombre de serveurs 12h vs Nombre total couv. 12h')
+    st.plotly_chart(fig5)
 
+    fig6 = px.scatter(df, x="Nbr serveurs 19h", y="Nbr total couv. 19h", title='Nombre de serveurs 19h vs Nombre total couv. 19h')
+    st.plotly_chart(fig6)
 
-# Si ce fichier est exécuté directement, appelez la fonction main()
+    # Indicateurs Clés
+    st.subheader("Indicateurs Clés")
+    total_couv = df["Nbr total couv."].sum()
+    mean_couv = df["Nbr total couv."].mean()
+    median_couv = df["Nbr total couv."].median()
+    st.write(f"Total couv.: {total_couv}")
+    st.write(f"Moyenne couv.: {mean_couv:.2f}")
+    st.write(f"Médiane couv.: {median_couv:.2f}")
+
+    # Filtres
+    st.subheader("Filtres")
+    jour_filter = st.selectbox("Choisir le jour:", df["Jour"].unique())
+    filtered_data = df[df["Jour"] == jour_filter]
+    st.write(filtered_data)
+
 if __name__ == "__main__":
     main()
