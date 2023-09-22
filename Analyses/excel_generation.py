da2c4f22ee875ead9e38331096153fa3b0548b3c
cedric lecerf <lecerf.c83@gmail.com>
2023-09-22 15:39:28 +0200

    maj

--- Analyses/excel_generation.py
+++ Analyses/excel_generation.py
@@ -86,7 +86,7 @@ def generate_excel_report(result_df, start_date, end_date, jours_moments_selecti
         jours_moments_selectionnes_text = dict_to_string_format(jours_moments_selectionnes)
         worksheet.merge_range('A8:E8', jours_moments_selectionnes_text, workbook.add_format({
             'text_wrap': True,
-            'font_size': 6,
+            'font_size': 8,
             'fg_color': 'E2E2E2',
             'font_color': 'black',
             'border': 1,
