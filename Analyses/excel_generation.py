1eb659e692e2972914ec959d34e2f9d0293a8ff0
cedric lecerf <lecerf.c83@gmail.com>
2023-09-22 15:38:32 +0200

    maj

--- Analyses/excel_generation.py
+++ Analyses/excel_generation.py
@@ -80,7 +80,7 @@ def generate_excel_report(result_df, start_date, end_date, jours_moments_selecti
         worksheet.set_column('E:E', len('Panier moyen €') + 2)
 
         def dict_to_string_format(jours_moments_dict):
-            return ' - '.join([f"{jour} : {', '.join(moments)}" for jour, moments in jours_moments_dict.items()])
+            return ' - '.join([f"{jour.upper()} : {', '.join(moments)}" for jour, moments in jours_moments_dict.items()])
 
 
         jours_moments_selectionnes_text = dict_to_string_format(jours_moments_selectionnes)
