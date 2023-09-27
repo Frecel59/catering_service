7b9cc9389b9e59c1d26aa59167fe7da774c12ee2
cedric lecerf <lecerf.c83@gmail.com>
2023-09-27 10:25:30 +0200

    maj

--- dashboard.py
+++ dashboard.py
@@ -352,14 +352,12 @@ def main():
         )
         st.plotly_chart(fig)
 
-    # Filtrage des données pour exclure les rangées où 'Météo 19h' est 'Inconnu'
-    filtered_df_report_19 = df_report[df_report['Météo 19h'] != 'Inconnu']
 
     # Graphique dans la colonne 2: Influence de la météo sur le nombre de couverts à 19h
     with col2:
         st.markdown("### Influence de la météo sur le nbr de couverts à 19h")
         fig = px.bar(
-            filtered_df_report_19.groupby('Méteo 19h')['Nbr total couv. 19h'].sum().reset_index(),
+            df_report.groupby('Méteo 19h')['Nbr total couv. 19h'].sum().reset_index(),
             x='Méteo 19h',
             y='Nbr total couv. 19h',
             color_discrete_sequence=[color_map_bar["Nbr total couv. 19h"]]
