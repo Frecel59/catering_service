9b86e14b371b5adc72c621bb5aaf034eb060103a
cedric lecerf <lecerf.c83@gmail.com>
2023-09-27 16:16:11 +0200

    MAJ

--- Analyses/bilan.py
+++ Analyses/bilan.py
@@ -1,32 +1,100 @@
+# Importation des bibliothèques nécessaires
+import streamlit as st
 import pandas as pd
 
-def analyses_bilan(jours_moments_selectionnes, filtered_vsd):
-    def calculate_totals(filtered_vsd, column_prefix):
-        totals = pd.Series(0, index=filtered_vsd.index)
-        for moment in ['12h', '19h']:
-            mask = filtered_vsd['Jour'].map(jours_moments_selectionnes).apply(lambda x: moment in x)
-            totals += filtered_vsd.loc[mask, f'{column_prefix} {moment}']
-        return totals
 
-    total_columns = [f'Total_{col}' for col in ['Couv', 'Couv_Off', 'CA', 'CA_Offerts']]
-    column_prefixes = ['Nbr couv.', 'Nbr couv. off', 'Additions', 'Additions off']
+def analyses_bilan (jours_moments_selectionnes, filtered_vsd):
+    # Créer une fonction générique pour calculer les totaux en fonction du
+    # jour et du moment sélectionnés
+    def calculate_total(row, column_name):
+        jour = row['Jour']
+        moments_selectionnes = jours_moments_selectionnes[jour]
 
-    for total_col, col_prefix in zip(total_columns, column_prefixes):
-        filtered_vsd[total_col] = calculate_totals(filtered_vsd, col_prefix)
+        total = 0
+        if 'Midi' in moments_selectionnes:
+            total += row[f'{column_name} 12h']
+        if 'Soir' in moments_selectionnes:
+            total += row[f'{column_name} 19h']
 
-    total_couv_selected = filtered_vsd['Total_Couv'].sum()
-    total_couv_off_selected = filtered_vsd['Total_Couv_Off'].sum()
+        return total
+
+    # Utiliser la fonction pour calculer le nbr de couverts à 12h
+    filtered_vsd['Nb_Couv_12h_Selected'] = filtered_vsd.apply(lambda row: \
+        calculate_total(row, 'Nbr couv.'), axis=1)
+
+    # Utiliser la fonction pour calculer le total de couverts
+    filtered_vsd['Total_Couv_Selected'] = filtered_vsd.apply(lambda row: \
+        calculate_total(row, 'Nbr total couv.'), axis=1)
+
+    # Utiliser la fonction pour calculer le total de couverts offerts
+    filtered_vsd['Total_Couv_Off_Selected'] = filtered_vsd.apply(lambda row: \
+        calculate_total(row, 'Nbr couv. off'), axis=1)
+
+    # Utiliser la fonction pour calculer le total CA
+    filtered_vsd['Total_CA_Selected'] = filtered_vsd.apply(lambda row: \
+        calculate_total(row, 'Additions'), axis=1)
+
+    # Utiliser la fonction pour calculer le total CA
+    filtered_vsd['Total_CA_Offerts_Selected'] = filtered_vsd.apply(lambda row: \
+        calculate_total(row, 'Additions off'), axis=1)
+
+
+    # Calculer la somme des couverts pour les jours et moments sélectionnés
+    total_couv_selected = filtered_vsd['Total_Couv_Selected'].sum()
+
+
+    # Calculer la somme des couverts offerts pour les jours et moments sélectionnés
+    total_couv_off_selected = filtered_vsd['Total_Couv_Off_Selected'].sum()
+
+
+     # Calculer la somme des couverts payants pour les jours et moments sélectionnés
     total_couv_payant_selected = total_couv_selected - total_couv_off_selected
 
-    percent_total_couv_off_selected = 100 * total_couv_off_selected / total_couv_selected if total_couv_selected != 0 else 0
-    percent_total_couv_payant_selected = 100 * total_couv_payant_selected / total_couv_selected if total_couv_selected != 0 else 0
 
-    total_ca_selected = filtered_vsd['Total_CA'].sum()
-    total_ca_offerts_selected = filtered_vsd['Total_CA_Offerts'].sum()
+    # Calculer le % des couverts offerts par rapport au total des couverts
+    if total_couv_selected != 0:
+        percent_total_couv_off_selected = (total_couv_off_selected \
+            / total_couv_selected) * 100
 
-    panier_moyen_selected = total_ca_selected / total_couv_selected if total_couv_selected != 0 else None
-    panier_moyen_payants_selected = total_ca_selected / total_couv_payant_selected if total_couv_payant_selected != 0 else None
-    panier_moyen_off_selected = total_ca_offerts_selected / total_couv_off_selected if total_couv_off_selected != 0 else None
+    else:
+        percent_total_couv_off_selected = 0
+
+
+    # Calculer le % des couverts payant par rapport au total des couverts
+    percent_total_couv_payant_selected = ((total_couv_selected - \
+        total_couv_off_selected) / total_couv_selected) * 100
+
+    # Calculer le CA pour les jours et moments sélectionnés
+    total_ca_selected = filtered_vsd['Total_CA_Selected'].sum()
+
+    # Calculer les offerts pour les jours et moments sélectionnés
+    total_ca_offerts_selected = filtered_vsd['Total_CA_Offerts_Selected'].sum()
+
+    # Calculer panier moyen total pour les jours et moments sélectionnés
+    if total_couv_selected != 0:
+        panier_moyen_selected = total_ca_selected / total_couv_selected
+
+    else:
+        total_couv_selected = 0
+        panier_moyen_selected = '-'
+
+    # Calculer panier moyen des payants pour les jours et moments sélectionnés
+    if total_couv_payant_selected != 0:
+        panier_moyen_payants_selected = total_ca_selected \
+            / total_couv_payant_selected
+
+    else:
+        total_couv_payant_selected = 0
+        panier_moyen_payants_selected = '-'
+
+    # Calculer panier moyen des offerts pour les jours et moments sélectionnés
+    if total_couv_off_selected != 0:
+        panier_moyen_off_selected = total_ca_offerts_selected \
+            / total_couv_off_selected
+
+    else:
+        total_couv_off_selected = 0
+        panier_moyen_off_selected = '-'
 
 
     # Créer un dictionnaire avec les résultats et les noms de colonnes
@@ -61,9 +129,12 @@ def analyses_bilan(jours_moments_selectionnes, filtered_vsd):
     result_df1 = result_df.set_index('Types')
 
     # Formater les colonnes du DataFrame
-    result_df1['Nbr Couverts'] = result_df1['Nbr Couverts'].apply(lambda x: f"{x:,}".replace(",", " "))
+    result_df1['Nbr Couverts'] = result_df1['Nbr Couverts'].apply\
+        (lambda x: f"{x:,}".replace(",", " "))
     result_df1['%'] = result_df1['%'].apply(lambda x: f"{x:.2f}")
-    result_df1['Total Additions €'] = result_df1['Total Additions €'].apply(lambda x: f"{x:,.2f}".replace(",", " "))
-    result_df1['Panier moyen €'] = result_df1['Panier moyen €'].apply(lambda x: f"{x:.2f}" if x is not None else '-')
+    result_df1['Total Additions €'] = result_df1['Total Additions €'].apply\
+        (lambda x: f"{x:,.2f}".replace(",", " "))
+    result_df1['Panier moyen €'] = result_df1['Panier moyen €'].apply\
+        (lambda x: f"{x:.2f}")
 
     return result_df, result_df1
