dd3a069e97b3af1b768ed3346c2ad72b00aad8fc
cedric lecerf <lecerf.c83@gmail.com>
2023-09-27 16:38:20 +0200

    MAJ

--- Analyses/bilan_n1.py
+++ Analyses/bilan_n1.py
@@ -4,21 +4,16 @@ import pandas as pd
 
 
 def analyses_bilan_n1 (jours_moments_selectionnes_a, filtered_a, filtered_a2):
-    # Créer une fonction générique pour calculer les totaux en fonction du
-    # jour et du moment sélectionnés
     def calculate_total(row, column_name):
         jour_a = row['Jour']
         moments_selectionnes = jours_moments_selectionnes_a[jour_a]
-
         total = 0
         if 'Midi' in moments_selectionnes:
             total += row[f'{column_name} 12h']
         if 'Soir' in moments_selectionnes:
             total += row[f'{column_name} 19h']
-
         return total
 
-    # Liste des colonnes d'origine et des nouvelles colonnes pour les totaux
     columns = {
         'Nbr total couv.': 'Nbr total couv.',
         'Additions': 'Total_CA_Selected',
@@ -26,38 +21,28 @@ def analyses_bilan_n1 (jours_moments_selectionnes_a, filtered_a, filtered_a2):
         'Additions off': 'Total_CA_Offerts_Selected'
     }
 
-    # Boucle sur les DataFrames et les colonnes
     for df in [filtered_a, filtered_a2]:
         for old_col, new_col in columns.items():
             df[new_col] = df.apply(lambda row: calculate_total(row, old_col), axis=1)
 
-
-    # 1. Identifier les colonnes pertinentes à analyser
     cols_to_analyze = [
         'Nbr total couv.', 'Total_CA_Selected',
         'Nbr couv. offerts', 'Total_CA_Offerts_Selected'
-
     ]
 
-    # 2. Calculer les totaux et les moyennes
     results = []
     for col in cols_to_analyze:
         total_n = filtered_a[col].sum()
         total_n1 = filtered_a2[col].sum()
-
         mean_n = filtered_a[col].mean()
         mean_n1 = filtered_a2[col].mean()
-
-        # 3. Calculer le pourcentage de variation
         total_variation = ((total_n - total_n1) / total_n1) * 100 if total_n1 != 0 else 0
-
         column_mapping = {
             'Nbr total couv.': 'Nbr total couv.',
             'Total_CA_Selected': 'Total Additions',
             'Nbr couv. offerts': 'Nbr couv. offerts',
             'Total_CA_Offerts_Selected': 'Total offerts'
         }
-
         results.append({
             'Indicateur': column_mapping[col],
             'Total N': total_n,
@@ -67,28 +52,23 @@ def analyses_bilan_n1 (jours_moments_selectionnes_a, filtered_a, filtered_a2):
             'Moyenne N-1': mean_n1,
         })
 
-    # Calculer le Panier Moyen pour N
-    panier_moyen_n = results[1]['Total N'] / results[0]['Total N'] if results[0]['Total N'] != 0 else 0
-    # Calculer le Panier Moyen pour N-1
-    panier_moyen_n1 = results[1]['Total N-1'] / results[0]['Total N-1'] if results[0]['Total N-1'] != 0 else 0
-    # Calculer la variation
-    panier_moyen_variation = ((panier_moyen_n - panier_moyen_n1) / panier_moyen_n1) * 100 if panier_moyen_n1 != 0 else 0
-    # Ajouter la ligne Panier Moyen à results
-    results.append({
+    # Panier Moyen calculation after 'Total Additions' row is appended
+    panier_moyen = {
         'Indicateur': 'Panier Moyen',
-        'Total N': panier_moyen_n,
-        'Total N-1': panier_moyen_n1,
-        'Variation (%)': panier_moyen_variation,
-        'Moyenne N': None,  # Ou une valeur appropriée si nécessaire
-        'Moyenne N-1': None,  # Ou une valeur appropriée si nécessaire
-    })
+        'Total N': results[1]['Total N'] / results[0]['Total N'] if results[0]['Total N'] != 0 else 0,
+        'Total N-1': results[1]['Total N-1'] / results[0]['Total N-1'] if results[0]['Total N-1'] != 0 else 0,
+        'Variation (%)': 0,  # You may need to calculate this manually
+        'Moyenne N': "-",
+        'Moyenne N-1': "-",
+    }
+    # Variation calculation for Panier Moyen
+    panier_moyen['Variation (%)'] = ((panier_moyen['Total N'] - panier_moyen['Total N-1']) / panier_moyen['Total N-1']) * 100 if panier_moyen['Total N-1'] != 0 else 0
 
-    # 4. Créer un dataframe pour afficher ces résultats
+    # Insert Panier Moyen row at the desired position
+    results.insert(2, panier_moyen)  # Adjust the index 2 to the desired position
+
     result_df_n1 = pd.DataFrame(results)
-
-    # Supprimer l'index par défaut du DataFrame
     result_df1_n1 = result_df_n1.set_index('Indicateur')
-
 
     # Appliquer le formatage ici
     def format_numbers(value):
