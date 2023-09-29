67a094ff85cc87c5afb4800804243567f422f831
cedric lecerf <lecerf.c83@gmail.com>
2023-09-29 14:54:50 +0200

    maj

--- analyses_n1.py
+++ analyses_n1.py
@@ -342,8 +342,7 @@ def main():
             x=df_report['Month'],
             y=df_report['Nbr couv. 12h'],
             name='N',
-            marker_color=color_map_bar_n["Nbr couv. 12h"],
-            opacity=0.6
+            marker_color=color_map_bar_n["Nbr couv. 12h"]
         )
     )
 
