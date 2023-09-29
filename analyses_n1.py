cbf89a1dad86ba0f543aa42c3bcd0b0ad07b7dd5
cedric lecerf <lecerf.c83@gmail.com>
2023-09-29 12:38:25 +0200

    maj

--- analyses_n1.py
+++ analyses_n1.py
@@ -327,22 +327,19 @@ def main():
 
     df_report['Période'] = 'N'
     df_report_n1['Période'] = 'N-1'
-    df_combined = pd.concat([df_report, df_report_n1])
-
 
     # Graphique dans la colonne 1: Tendance des couverts à 12h
     with col1:
         st.markdown("### Tendance des couverts à 12h")
         fig = px.line(
-            df_combined,
+            df_report,
             x='Date',
             y='Nbr total couv. 12h',
-            color='Période', # 'Période' existe maintenant dans df_combined
-            color_discrete_sequence=[color_map_bar_n["Nbr couv. 12h"], color_map_bar_n1["Nbr couv. 12h"]],
-            labels={'Période':'Période', 'Date':'Date', 'Nbr total couv. 12h':'Nombre total de couverts à 12h'}
+            color_discrete_sequence=[color_map_bar_n["Nbr total couv. 12h"]]
         )
         fig.update_xaxes(tickformat="%d-%m-%y")
         st.plotly_chart(fig)
+
 
 
 
