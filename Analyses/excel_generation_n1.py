0390508e51a686a171de2b70fe2a511b8787b8c8
cedric lecerf <lecerf.c83@gmail.com>
2023-09-29 11:06:05 +0200

    config telecharger bilan en excel

--- Analyses/excel_generation_n1.py
+++ Analyses/excel_generation_n1.py
@@ -24,7 +24,7 @@ def generate_excel_report_n1(result_df_n1, start_date_a, end_date_a, start_date_
         worksheet.set_zoom(130)
 
         # Titre
-        title_text = f"N : du {start_date_a} au {end_date_a} - N-1 : du {start_date_a2} au {end_date_a2}"
+        title_text = f"N : du {start_date_a} au {end_date_a} vs N-1 : du {start_date_a2} au {end_date_a2}"
         worksheet.merge_range('A1:F1', title_text, workbook.add_format({
             'bold': True,
             'font_size': 15,
@@ -75,12 +75,12 @@ def generate_excel_report_n1(result_df_n1, start_date_a, end_date_a, start_date_
 
 
         # Définition de la largeur pour chaque colonne
-        worksheet.set_column('A:A', len('Indicateur') + 8)
-        worksheet.set_column('B:B', len('N') + 12)
-        worksheet.set_column('C:C', len('N-1') + 10)
-        worksheet.set_column('D:D', len('Variation') + 10)
-        worksheet.set_column('E:E', len('Moyenne N') + 6)
-        worksheet.set_column('F:F', len('Moyenne N-1') + 6)
+        worksheet.set_column('A:A', len('Indicateur') + 7)
+        worksheet.set_column('B:B', len('N') + 14)
+        worksheet.set_column('C:C', len('N-1') + 14)
+        worksheet.set_column('D:D', len('Variation') + 3)
+        worksheet.set_column('E:E', len('Moyenne N') + 3)
+        worksheet.set_column('F:F', len('Moyenne N-1') + 3)
 
         def dict_to_string_format(jours_moments_dict):
             text_list = [f"{jour.upper()} : {', '.join(moments)}" for jour, moments in jours_moments_dict.items() if moments]
